f5cdffb0eca4992516ce3b417c90eedea256bcea
Ashish Nevan <nevchris242@gmail.com>
2024-05-09 17:02:11 -0400

    fix deploy wip

--- src/app/listings/page.tsx
+++ src/app/listings/page.tsx
@@ -1,13 +1,15 @@
-import Listing from "@/app/(models)/Listing";
 import ListingDisplay from "@/app/(components)/ListingDisplay";
 import { getAllListings, ListingData } from "@/app/lib/listingActions";
 
 export default async function listings() {
   const listingsData: ListingData = await getAllListings();
 
-  if (listingsData.error || !listingsData.data) {
+  if (listingsData.error) {
     throw new Error("Error fetching listings");
   } else {
+    if (!listingsData.data) {
+      return <div>No listings found</div>;
+    }
     return (
       <div className="h-full mx-auto py-4 w-full">
         <ListingDisplay listings={listingsData.data} />
