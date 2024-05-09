ff785c3ce9eb67af2ae18941cfaedbfa03fc3a12
Ashish Nevan <nevchris242@gmail.com>
2024-05-09 16:52:50 -0400

    fixed err in listings/

--- src/app/listings/page.tsx
+++ src/app/listings/page.tsx
@@ -3,9 +3,6 @@ import ListingDisplay from "@/app/(components)/ListingDisplay";
 import { getAllListings, ListingData } from "@/app/lib/listingActions";
 
 export default async function listings() {
-  const getListings = async () => {};
-  const listings = await getListings();
-  // console.log(listings);
   const listingsData: ListingData = await getAllListings();
 
   if (listingsData.error || !listingsData.data) {
