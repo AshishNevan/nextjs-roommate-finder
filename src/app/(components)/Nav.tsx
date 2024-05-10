111a8da7c6736d9394e101776470a4c195157335
Ashish Nevan <nevchris242@gmail.com>
2024-05-10 09:14:41 -0400

    ui wip

--- src/app/(components)/Nav.tsx
+++ src/app/(components)/Nav.tsx
@@ -1,21 +1,101 @@
 import Link from "next/link";
 import { createClient } from "@/utils/supabase/server";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Tooltip,
+  TooltipTrigger,
+  TooltipProvider,
+  TooltipContent,
+} from "@/components/ui/tooltip";
+import { Button } from "@/components/ui/button";
 
 const Nav = async () => {
   const supabase = createClient();
   const { data, error } = await supabase.auth.getUser();
+  // console.log(data);
   return (
-    <div className="nav border border-black">
-      <div className="col-span-1">something</div>
-      <div className="col-span-3 text-center">logo</div>
-      {data && data.user && data.user.email ? (
-        <div className="col-span-1 text-right">{data.user.email}</div>
-      ) : (
-        <Link href="/login">
-          <div className="col-span-1 text-right">Login</div>
-        </Link>
-      )}
-    </div>
+    <nav className="flex items-center justify-between px-4 py-2 bg-white dark:bg-gray-800">
+      <div className="flex items-center space-x-4">
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <div className="flex flex-col space-y-2">
+              <DropdownMenuSeparator />
+              <Link href={"/"}>
+                <Button variant={"link"}>Home</Button>
+              </Link>
+              <DropdownMenuSeparator />
+              <Link href={"/listings"}>
+                <Button variant={"link"}>View Listings</Button>
+              </Link>
+              <DropdownMenuSeparator />
+              <Link href={"/createListing"}>
+                <Button variant={"link"}>Create Listing</Button>
+              </Link>
+              <DropdownMenuSeparator />
+            </div>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <Avatar>
+          <AvatarImage src="https://example.com/user.png" alt="User Profile" />
+        </Avatar>
+      </div>
+      <div className="flex items-center space-x-4">
+        {data.user?.email ? (
+          <Link href={`/users/${data.user.id}`}>
+            <Avatar>
+              <AvatarImage
+                src="https://example.com/user.png"
+                alt="User Profile"
+              />
+              <AvatarFallback>
+                {data.user?.email?.charAt(0).toLocaleUpperCase()}
+              </AvatarFallback>
+            </Avatar>
+          </Link>
+        ) : (
+          <Link href="/login">
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Avatar>
+                    <AvatarImage
+                      src="https://example.com/user.png"
+                      alt="User Profile"
+                    />
+                    <AvatarFallback>{"?"}</AvatarFallback>
+                  </Avatar>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Login</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </Link>
+        )}
+      </div>
+    </nav>
   );
 };
 export default Nav;
--- src/app/listings/[id]/page.tsx
+++ src/app/listings/[id]/page.tsx
@@ -1,6 +1,16 @@
 import Listing from "@/app/(models)/Listing";
 import { ListingData, getListingById } from "@/app/lib/listingActions";
 import Image from "next/image";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Button } from "@/components/ui/button";
 
 export default async function property({ params }: { params: { id: string } }) {
   if (!params.id) {
@@ -13,49 +23,76 @@ export default async function property({ params }: { params: { id: string } }) {
     const property: Listing = data![0];
     return (
       <div className="h-screen flex flex-col items-center">
-        <div className="w-full h-1/2">
-          {property.images && property.images[0] ? (
-            <Image
-              className="object-fill h-40 w-full"
-              src={property.images[0]}
-              alt=""
-              width={500}
-              height={500}
-            />
-          ) : (
-            <div className="h-full flex justify-center items-center">
-              No Image Available
-            </div>
-          )}
-        </div>
-        <div className="grid sm: grid-cols-1 md:grid-cols-4 gap-x-2 mx-2">
-          <div className="col-span-3 py-4">
+        {property.images.length > 0 ? (
+          <Carousel className="w-full max-w-md">
+            <CarouselContent>
+              {property.images.map((_, index) => (
+                <CarouselItem key={index}>
+                  <div className="p-1">
+                    <Card>
+                      <CardContent className="flex aspect-square items-center justify-center p-6">
+                        <AspectRatio ratio={1 / 1}>
+                          <Image
+                            src={property.images[index]}
+                            alt=""
+                            fill
+                            objectFit="cover"
+                          />
+                        </AspectRatio>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        ) : (
+          <div className="h-full flex justify-center items-center">
+            <p>Image Unavailable</p>
+          </div>
+        )}
+        <div className="grid sm:grid-cols-1 lg:grid-cols-3 gap-2 mx-2">
+          <div className="col-span-2 py-4">
             <div className="flex flex-row justify-between">
               <div className="w-3/4">
-                <h1 className="font-semibold text-gray-900">
-                  {property.location.address}
-                </h1>
+                <h1>{property.location.address}</h1>
               </div>
               <div className="flex flex-row gap-x-2">
-                <button>favorite</button>
-                <button>share</button>
+                <button>
+                  <h3>🖤</h3>
+                </button>
+                <button>
+                  <h3>📎</h3>
+                </button>
               </div>
             </div>
-            <h2 className="font-semibold text-gray-900 p-2">
-              {property.title}
-            </h2>
-            <hr className="border-gray-400 mx-2" />
-            <p className="font-semibold text-gray-900 p-2">
-              Description: {property.description}
-            </p>
+            <h2>{property.title}</h2>
+            <p>Description: {property.description}</p>
           </div>
-          <div className="col-span-1 border border-black h-full py-4">
-            <h3 className="text-center">Contact</h3>
-            <div className="flex flex-col h-2/3 justify-center">
-              <button className="border border-black mx-4 rounded-lg hover:bg-blue-500">
-                Apply to be a roommate
-              </button>
-            </div>
+          <div className="col-span-1 flex flex-col m-2 gap-2">
+            <Card className="h-32 my-auto justify-items-center">
+              <div className="flex flex-col justify-evenly h-full">
+                <CardTitle className="text-center">Interested?</CardTitle>
+                <CardContent className="mx-auto">
+                  <Button>Apply to be a roommate</Button>
+                </CardContent>
+              </div>
+            </Card>
+            <Card>
+              <CardContent>
+                <h3 className="text-center">Map</h3>
+                <div className=" aspect-square">
+                  <iframe
+                    src={`https://maps.google.com/maps?q=${property.location.address},${property.location.city}&z=15&output=embed`}
+                    width="100%"
+                    height="100%"
+                    allowFullScreen
+                  />
+                </div>
+              </CardContent>
+            </Card>
           </div>
         </div>
       </div>
