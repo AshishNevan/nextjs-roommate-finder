9c3ad66bdab666dfb3e5a90ced572f0b22c6cf76
Ashish Nevan <nevchris242@gmail.com>
2024-05-16 01:02:41 -0500

    fixed main tag in listings/[id]

--- src/app/listings/[id]/page.tsx
+++ src/app/listings/[id]/page.tsx
@@ -22,80 +22,82 @@ export default async function property({ params }: { params: { id: string } }) {
   } else {
     const property: Listing = data![0];
     return (
-      <div className="h-screen flex flex-col items-center">
-        {property.images.length > 0 ? (
-          <Carousel className="w-full max-w-md">
-            <CarouselContent>
-              {property.images.map((_, index) => (
-                <CarouselItem key={index}>
-                  <div className="p-1">
-                    <Card>
-                      <CardContent className="flex aspect-square items-center justify-center p-6">
-                        <AspectRatio ratio={1 / 1}>
-                          <Image
-                            src={property.images[index]}
-                            alt=""
-                            fill
-                            objectFit="cover"
-                          />
-                        </AspectRatio>
-                      </CardContent>
-                    </Card>
-                  </div>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-          </Carousel>
-        ) : (
-          <div className="h-full flex justify-center items-center">
-            <p>Image Unavailable</p>
-          </div>
-        )}
-        <div className="grid sm:grid-cols-1 lg:grid-cols-3 gap-2 mx-2">
-          <div className="col-span-2 py-4">
-            <div className="flex flex-row justify-between">
-              <div className="w-3/4">
-                <h1>{property.location.address}</h1>
-              </div>
-              <div className="flex flex-row gap-x-2">
-                <button>
-                  <h3>🖤</h3>
-                </button>
-                <button>
-                  <h3>📎</h3>
-                </button>
-              </div>
+      <main>
+        <div className="flex flex-col items-center">
+          {property.images.length > 0 ? (
+            <Carousel className="w-full max-w-md">
+              <CarouselContent>
+                {property.images.map((_, index) => (
+                  <CarouselItem key={index}>
+                    <div className="p-1">
+                      <Card>
+                        <CardContent className="flex aspect-square items-center justify-center p-6">
+                          <AspectRatio ratio={1 / 1}>
+                            <Image
+                              src={property.images[index]}
+                              alt=""
+                              fill
+                              objectFit="cover"
+                            />
+                          </AspectRatio>
+                        </CardContent>
+                      </Card>
+                    </div>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
+          ) : (
+            <div className="h-full flex justify-center items-center">
+              <p>Image Unavailable</p>
             </div>
-            <h2>{property.title}</h2>
-            <p>Description: {property.description}</p>
-          </div>
-          <div className="col-span-1 flex flex-col m-2 gap-2">
-            <Card className="h-32 my-auto justify-items-center">
-              <div className="flex flex-col justify-evenly h-full">
-                <CardTitle className="text-center">Interested?</CardTitle>
-                <CardContent className="mx-auto">
-                  <Button>Apply to be a roommate</Button>
-                </CardContent>
-              </div>
-            </Card>
-            <Card>
-              <CardContent>
-                <h3 className="text-center">Map</h3>
-                <div className=" aspect-square">
-                  <iframe
-                    src={`https://maps.google.com/maps?q=${property.location.address},${property.location.city}&z=15&output=embed`}
-                    width="100%"
-                    height="100%"
-                    allowFullScreen
-                  />
+          )}
+          <div className="grid sm:grid-cols-1 lg:grid-cols-3 gap-2 mx-2">
+            <div className="col-span-2 py-4">
+              <div className="flex flex-row justify-between">
+                <div className="w-3/4">
+                  <h1>{property.location.address}</h1>
                 </div>
-              </CardContent>
-            </Card>
+                <div className="flex flex-row gap-x-2">
+                  <button>
+                    <h3>🖤</h3>
+                  </button>
+                  <button>
+                    <h3>📎</h3>
+                  </button>
+                </div>
+              </div>
+              <h2>{property.title}</h2>
+              <p>Description: {property.description}</p>
+            </div>
+            <div className="col-span-1 flex flex-col m-2 gap-2">
+              <Card className="h-32 my-auto justify-items-center">
+                <div className="flex flex-col justify-evenly h-full">
+                  <CardTitle className="text-center">Interested?</CardTitle>
+                  <CardContent className="mx-auto">
+                    <Button>Apply to be a roommate</Button>
+                  </CardContent>
+                </div>
+              </Card>
+              <Card>
+                <CardContent>
+                  <h3 className="text-center">Map</h3>
+                  <div className=" aspect-square">
+                    <iframe
+                      src={`https://maps.google.com/maps?q=${property.location.address},${property.location.city}&z=15&output=embed`}
+                      width="100%"
+                      height="100%"
+                      allowFullScreen
+                    />
+                  </div>
+                </CardContent>
+              </Card>
+            </div>
           </div>
         </div>
-      </div>
+      </main>
     );
   }
 }
